c78e7da9727f44250b0f6c0ff67c0cfbc17322df
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2021-11-06 15:31:58 -0600

    Create Loadstring.lua

---
+++ Notifications/Loadstring.lua
@@ -0,0 +1,6 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxa/Libraries/main/Notifications/Source.lua", true))();
+lib.configuration = {
+    TextColor3 = Color3.fromRGB(255, 255, 255), -- https://www.rapidtables.com/web/color/RGB_Color.html
+    TextSize = 14, -- any number you want
+    Font = "Code", -- https://developer.roblox.com/en-us/api-reference/enum/Font
+}
