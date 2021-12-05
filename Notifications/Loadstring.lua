d849d400553010b7155e747971a527476df71616
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2021-12-05 01:42:14 -0600

    Update Loadstring.lua

--- Notifications/Loadstring.lua
+++ Notifications/Loadstring.lua
@@ -1,6 +1,10 @@
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxa/Libraries/main/Notifications/Source.lua", true))();
-lib.configuration = {
-    TextColor3 = Color3.fromRGB(255, 255, 255), -- https://www.rapidtables.com/web/color/RGB_Color.html
-    TextSize = 14, -- any number you want
-    Font = "Code", -- https://developer.roblox.com/en-us/api-reference/enum/Font
+-- for usage, please refer to the docs, https://github.com/xaxaxaxaxaxaxaxaxa/Libraries/edit/main/Notifications/Documentation.lua
+local source = loadstring(game:HttpGet("https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxa/Libraries/main/Notifications/Source.lua", true))();
+source.configuration = {
+    FadeInTime = 0.25, -- The time it takes for the notification elements to fade into your visibility when you call the Notify function
+    FadeOutTime = 0.1, -- The time it takes for the notification elements to fade out once the notification is over
+        
+    Font = "Code", -- The font of all the text in the notification, https://developer.roblox.com/en-us/api-reference/enum/Font
+    MainColor = Color3.fromRGB(170, 0, 255), -- The main color of the notification frame, https://www.rapidtables.com/web/color/RGB_Color.html
+    BackgroundColor = Color3.fromRGB(27, 27, 27), The background color of the notification frame, https://www.rapidtables.com/web/color/RGB_Color.html
 }
