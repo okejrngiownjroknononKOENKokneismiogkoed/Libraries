fd55fa0a47ea169d1690fa2a2f02db6eb2997714
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2021-12-05 01:40:25 -0600

    Update Documentation.lua

--- Notifications/Documentation.lua
+++ Notifications/Documentation.lua
@@ -1,21 +1,24 @@
 --[[
 Documentation for: https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxa/Libraries/main/Notifications/Source.lua
-Updated as of: 11/6/2021
+Updated as of: 12/5/2021
 ===============================================================================================================
 
 local source = loadstring(game:HttpGet("https://raw.githubusercontent.com/xaxaxaxaxaxaxaxaxa/Libraries/main/Notifications/Source.lua", true))();
 source.configuration = {
-    TextColor3 = Color3.fromRGB(255, 255, 255), -- https://www.rapidtables.com/web/color/RGB_Color.html
-    TextSize = 14, -- any number you want
-    Font = "Code", -- https://developer.roblox.com/en-us/api-reference/enum/Font
+    FadeInTime = 0.25, -- The time it takes for the notification elements to fade into your visibility when you call the Notify function
+    FadeOutTime = 0.1, -- The time it takes for the notification elements to fade out once the notification is over
+        
+    Font = "Code", -- The font of all the text in the notification, https://developer.roblox.com/en-us/api-reference/enum/Font
+    MainColor = Color3.fromRGB(170, 0, 255), -- The main color of the notification frame, https://www.rapidtables.com/web/color/RGB_Color.html
+    BackgroundColor = Color3.fromRGB(27, 27, 27), The background color of the notification frame, https://www.rapidtables.com/web/color/RGB_Color.html
 }
 
-local lib = source.new()
+local lib = source.importmodule()
 ========================
 
 Custom functions = [
     Notify = [
-        lib.Notify(Title<string>, Description<string>, Duration<number>, Settings<table>)
+        lib.Notify(Title<string>, Description<string>, Duration<number>, Configuration<table>)
         
         Example - lib.Notify("ciazware", "Test", 3, source.configuration)
     ]
