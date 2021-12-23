4b8c41b7860476298e8672c2242d0173e9de9b25
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2021-12-23 17:00:06 -0600

    Update Documentation.lua

--- Chalk/Documentation.lua
+++ Chalk/Documentation.lua
@@ -4,6 +4,8 @@ local chalk = loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/x
 -- Call the "colorprint" function
 chalk.colorprint("Your message here", {R = ColorHere, G = ColorHere, B = ColorHere});
 
+-- Now press F9 on your keyboard and will say what you printed
+
 --[[ Example
 
 chalk.colorprint("Hello World", {R = 255, G = 12, B = 10});
